c129c8ce99729e3bdbc8f732d1b1622faa41760c
AnggiWaSa <anggiwasa@gmail.com>
2023-11-02 11:34:20 +0700

    add reponsiv

--- src/main.js
+++ src/main.js
@@ -13,7 +13,11 @@ generateDigitalClok = () => {
     let menit = `0${date.getMinutes()}`.substr(-2);
     let detik = `0${date.getSeconds()}`.substr(-2);
 
-    document.getElementById('clock-digital').innerHTML = `${jam}:${menit}:${detik}`;
+    let tahun = `0${date.getFullYear()}`.substr(-2);
+    let bulan = `0${date.getMonth()}`.substr(-2);
+    let hari = `0${date.getDate()}`.substr(-2);
+
+    document.getElementById('clock-digital').innerHTML = `<span class="cd-date">${hari}/${bulan}/${tahun}</span> | <span class="cd-time">${jam}:${menit}:${detik}</span>`;
 }
 
 setInterval(() => {
